ac6fc629eb57148735e9a647a9ccc350b80979ee
zhangxing0716 <zhangxing425683232@qq.com>
2017-02-28 03:10:31 +0800

    foot修改

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -4,18 +4,35 @@
  * @since V1.0.0 2017-2-27
 */
 
-define(['jquery', 'text!Footer.html'], function($, FooterTpl){
-	return {
-		render:function() {
-			var css = '<link rel="stylesheet" type="text/css" href="./app/components/Footer/Footer.css">';
-			var strHTML = '<div class="foot-toolbar">';
-			strHTML += '<div class="foot-toolbar-inner">';
-			strHTML += '<a href="#mainView" class="foot-tab-link active"><i class="ion ion-ios-home"></i><label>主页</label></a>';
-			strHTML += '<a href="#subjectView" class="foot-tab-link"><i class="ion ion-star"></i><label>动态</label></a>';
-			strHTML += '<a href="#caseView" class="foot-tab-link"><i class="ion ion-android-contacts"></i><label>好友</label></a>';
-			strHTML += '<a href="#settingView" class="foot-tab-link"><i class="ion ion-android-person"><span class="badge">1</span></i><label>我的</label></a>';
-			strHTML += '</div></div>';
-			return css+strHTML;
-		}
-	}
+define([
+ 'backbone',
+ 'underscore', 
+ 'text!Footer.html',
+ 'text!Footer.css'], function(Backbone, _, FooterTpl, FooterCss){
+    var Footer = Backbone.View.extend({
+
+      events:{
+        'click .foot-tab-link': 'handleClick'
+      },
+
+      initialize: function () {
+        Backbone
+          .$('<style type="text/css"></style>')
+          .html(FooterCss)
+          .prependTo(document.head);
+      },
+
+      handleClick: function (ev) {
+        alert(ev.currentTarget.href);
+      },
+
+      render: function () {
+        return this.$el.html(_.template(FooterTpl)({ title1: '主页', title2: '动态', title3: '好友', title4: '我的', badge:4}));
+      }
+
+    });
+
+    return new Footer({
+      className: 'foot-toolbar'
+    });
 });
